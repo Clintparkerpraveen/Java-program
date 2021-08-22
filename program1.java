1e6141152f2108469a999615e43acbb0db3ee029
praveen <88870005+Praveen-ranganathan@users.noreply.github.com>
2021-08-22 14:55:59 +0530

    Update and rename program2.java to program1.java

--- program1.java
+++ program1.java
@@ -1,9 +1,9 @@
-class Student1{
+class program1{
    int id;//data member (also instance variable)
    String name;//data member(also instance variable)
   
    public static void main(String args[]){
-   Student1 s1=new Student1();//creating an object of Student
+   program1 s1=new program1();//creating an object of Student
    System.out.println(s1.id);
    System.out.println(s1.name);
    }
