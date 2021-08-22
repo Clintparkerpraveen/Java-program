990d0d0ab085f46f34faec99814e456d37af55f3
praveen <88870005+Praveen-ranganathan@users.noreply.github.com>
2021-08-22 15:28:43 +0530

    Update students.java

--- students.java
+++ students.java
@@ -1,16 +1,16 @@
-//Maintains the records of students
+//MAINTAINS THE RECORDS OF STUDENTS
 class students{
 	int rollno;
 	String name;
-	void insertRecord(int r,String n){   //creating record
+	void insertRecord(int r,String n){   //CREATING RECORD
 		rollno=r;
 		name=n;
 	}
-	void displayInformation(){System.out.println(rollno+" "+name);}  //Displays all the values in record
+	void displayInformation(){System.out.println(rollno+" "+name);}  //DISPLAYS ALL THE VALUES IN RECORDS
 	public static void main(String args[]){
 		students s1=new students();
 		students s2=new students();
-		s1.insertRecord(101,"clint");	  //insert values in record		
+		s1.insertRecord(101,"clint");	  //INSERT VALUES IN RECORD		
 		s2.insertRecord(102,"parker");
 		s1.displayInformation();
 		s2.displayInformation();
