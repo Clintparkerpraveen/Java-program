8a5b4e8478d052de31b4a5228dcd3876aa35a823
praveen <88870005+Praveen-ranganathan@users.noreply.github.com>
2021-08-22 16:03:51 +0530

    Add files via upload

---
+++ Rectangle2.java
@@ -0,0 +1,17 @@
+
+class Rectangle2{
+	int length;
+	int width;
+	void insert(int l,int w){
+		length=l;
+		width=w;
+	}
+	void calculateArea(){System.out.println(length*width);}
+	public static void main(String args[]){
+		Rectangle2 r1=new Rectangle2(),r2=new Rectangle2();  //creating two objects on one type
+		r1.insert(3,5);
+		r2.insert(7,9);
+		r1.calculateArea();
+		r2.calculateArea();
+	}
+}
